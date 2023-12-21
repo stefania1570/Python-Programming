d74f67be5bfbd6dd15e963a24bbf04bcb31166b0
stefania1570 <stefaniabianca1570@yahoo.com>
2023-12-21 11:52:27 +0200

    done

--- PROIECT B - PARTITION STATISTICS/proj.py
+++ PROIECT B - PARTITION STATISTICS/proj.py
@@ -4,17 +4,18 @@
 #THIRD STEP: MAKE A DICTIONARY IN WHICH THE KEYS ARE THE EXTENSIONS AND THE VALUES ARE THE FREQUENCIES OF THE EXTENSIONS -- DONE
 #3.1: CALCULATE (AND PRINT) THE PROPORTION OF EACH FILE TYPE (EACH EXTENSION) AS A NUMBER -- DONE
 #3.2: + its size (for each extension what is the size occupied) -- DONE
-#FOURTH STEP: MAKE THE BAR PLOT USING MATPLOTLIB
-#FIFTH STEP: EXTEND IT TO A PARTITION (pana aici testez doar pe un folder ca sa vad daca e bine ce face)
+#FOURTH STEP: MAKE THE BAR PLOT USING MATPLOTLIB -- DONE
+#FIFTH STEP: EXTEND IT TO A PARTITION (pana aici testez doar pe un folder ca sa vad daca e bine ce face) -- DONE
 
 import os
+import matplotlib.pyplot as matplt
 
 foldersNumber = 0
 filesNumber = 0
 extension_dict = {}
 size_dict = {}
 
-for (root, directories, files) in os.walk("C:\\Users\\Stefania\\OneDrive\\Desktop\\Proiect"):
+for (root, directories, files) in os.walk("D:\\"): #C:\\Users\\Stefania\\OneDrive\\Desktop\\Proiect
     foldersNumber += len(directories)
     filesNumber += len(files)
 
@@ -24,8 +25,9 @@ for (root, directories, files) in os.walk("C:\\Users\\Stefania\\OneDrive\\Deskto
 
         file_path = os.path.join(root, fullFileName)
         file_size = os.path.getsize(file_path)
+        file_size_in_kb = file_size / 1024 # Convert bytes to kilobytes
 
-        size_dict[file_extension] = size_dict.get(file_extension, 0) + file_size
+        size_dict[file_extension] = size_dict.get(file_extension, 0) + file_size_in_kb 
 
 print(f"Total number of subfolders: {foldersNumber}")
 print(f"Total number of files: {filesNumber}")
@@ -33,9 +35,33 @@ print(f"Total number of files: {filesNumber}")
 print("\nExtension frequencies and proportions:")
 for extension, frequency in extension_dict.items():
     proportion = (frequency / filesNumber) * 100
-    print(f"{extension}: {frequency} file(s) ({proportion:.2f}% of total)")
+    print(f"{extension}: {frequency} files ({proportion:.2f}% of total)")
 
 print("\nExtension sizes:")
 for extension, size in size_dict.items():
-    size_in_kb = size / 1024  # Convert bytes to kilobytes
-    print(f"{extension}: {size_in_kb:.2f} KB")
+    print(f"{extension}: {size:.2f} KB")
+
+# Extensions Frequency Bar Plot
+frequency_labels = list(extension_dict.keys())
+frequency_values = list(extension_dict.values())
+
+matplt.figure(figsize=(10, 6))
+matplt.bar(frequency_labels, frequency_values, color ='maroon', width = 0.6)
+matplt.xlabel('File Type')
+matplt.ylabel('Frequency')
+matplt.title('File Type Frequencies')
+matplt.xticks(rotation=60, ha='right')  # rotim etichetele de pe axa ox ca sa se vada mai bine
+matplt.show()
+
+# Extension sizes
+size_labels = list(size_dict.keys())
+size_values = list(size_dict.values())
+
+# Plotting the bar chart for extension sizes
+matplt.figure(figsize=(10, 6))
+matplt.bar(size_labels, size_values, color ='maroon', width = 0.6)
+matplt.xlabel('File Extension')
+matplt.ylabel('Size (KB)')
+matplt.title('File Extension Sizes')
+matplt.xticks(rotation=60, ha='right')  # rotim etichetele de pe axa ox ca sa se vada mai bine
+matplt.show()
